eba9c1981d5fbc98012500c5fba10c92d0af345b
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2022-12-01 00:52:04 -0400

    modelos storages & tracks creados

---
+++ models/noSQL/tracks.js
@@ -0,0 +1,44 @@
+const mongoose = require(mongoose);
+
+const TracksScheme = new mongoose.Schema(
+	{
+		name: {
+			type: String,
+		},
+		album: {
+			type: String,
+		},
+		cover: {
+			type: String,
+			validate: (req) => {
+				return true;
+			},
+			message: 'ERROR_URL',
+		},
+		artist: {
+			name: {
+				type: String,
+			},
+			nickname: {
+				type: String,
+			},
+			nationality: {
+				type: String,
+			},
+		},
+		duration: {
+			start: {
+				type: Number,
+			},
+		},
+		mediaId: {
+			type: mongoose.Types.ObjectId,
+		},
+	},
+	{
+		timestamps: true, //TODO: createdAT, updatedAt
+		versionKey: false,
+	}
+);
+
+module.exports = mongoose.model('tracks', TracksScheme);
