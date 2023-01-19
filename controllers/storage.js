60da8fc39a71edcc5bd66a53fa404b10fd9692f2
Cristian Carrillo <cristiancarrillo311087@gmail.com>
2023-01-19 19:11:25 -0400

    eliminación de item por ID en el storage configurado

--- controllers/storage.js
+++ controllers/storage.js
@@ -1,17 +1,33 @@
+const fs = require('fs');
+const {matchedData} = require('express-validator');
 const {storageModel} = require('../models');
+const {handleHttpError} = require('../utils/handleError');
 
 const PUBLIC_URL = process.env.PUBLIC_URL;
+const MEDIA_PATH = `${__dirname}/../storage`;
 
 // obteniendo lista
 const getItems = async (req, res) => {
-	// res.json({ msg: 'mandando respuesta' });
-	const data = await storageModel.find({});
-
-	res.send({data});
+	try {
+		const data = await storageModel.find({});
+		res.send({data});
+	} catch (err) {
+		console.log(err);
+		handleHttpError(res, 'ERROR_LIST_ITEMS');
+	}
 };
 
 //obteniendo un detalle
-const getItem = (req, res) => {};
+const getItem = async (req, res) => {
+	try {
+		const {id} = matchedData(req);
+		const data = await storageModel.findById(id);
+		res.send({data});
+	} catch (err) {
+		console.log(err);
+		handleHttpError(res, 'ERROR_GET_DETAIL_ITEM');
+	}
+};
 
 //insertar un registro
 const createItem = async (req, res) => {
@@ -29,7 +45,27 @@ const createItem = async (req, res) => {
 const updateItem = (req, res) => {};
 
 //eliminar un registro
-const deleteItem = (req, res) => {};
+const deleteItem = async (req, res) => {
+	try {
+		// req = matchedData(req);
+		// const id = req.id;
+		const {id} = matchedData(req);
+		const findMedia = await storageModel.findById(id);
+		const fileName = findMedia.filename;
+		await storageModel.delete({_id: id});
+		fs.unlinkSync(`${MEDIA_PATH}/${fileName}`);
+
+		const data = {
+			findMedia: fileName,
+			deleted: true,
+		};
+
+		res.send({data});
+	} catch (err) {
+		console.log(err);
+		handleHttpError(res, 'ERROR_DELETE_ITEMS');
+	}
+};
 
 module.exports = {
 	getItems,
--- routes/storage.js
+++ routes/storage.js
@@ -1,10 +1,27 @@
 const express = require('express');
+const {validatorGetItem} = require('../validators/storage');
 const uploadMIddleware = require('../utils/handleStorage');
-const {createItem} = require('../controllers/storage');
+const {
+	createItem,
+	getItem,
+	getItems,
+	updateItem,
+	deleteItem,
+} = require('../controllers/storage');
 
 const router = express.Router();
 //TODO: localhost:3000/api/storage
 
+// Create an Item
 router.post('/', uploadMIddleware.single('myfile'), createItem);
+
+//Get all the items
+router.get('/', getItems);
+
+//Get an Item
+router.get('/:id', validatorGetItem, getItem);
+
+//Delete an Item
+router.delete('/:id', validatorGetItem, deleteItem);
 
 module.exports = router;
